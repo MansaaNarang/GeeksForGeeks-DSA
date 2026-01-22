5e4d2b925f85e94b10c5925f3f2145aaf02236da
Mansaa Narang <142112077+MansaaNarang@users.noreply.github.com>
2026-01-22 15:13:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of paths/number-of-paths.java
+++ Difficulty: Medium/Number of paths/number-of-paths.java
@@ -7,18 +7,18 @@ class Solution {
         return 1;
         if(dp[m][n]!=-1)
         return dp[m][n];
-        int rightWays=paths(m,n-1);
-        int downWays=paths(m-1,n);
-        return dp[m][n]=rightWays+downWays;
+        int rightways=paths(m,n-1);
+        int downways=paths(m-1,n);
+        return dp[m][n]=rightways+downways;
     }
     public int numberOfPaths(int m, int n) {
-        // Code Here
+        // code here
         dp=new int[m+1][n+1];
         for(int i=0;i<=m;i++){
-        for(int j=0;j<=n;j++){
-            dp[i][j]=-1;
+            for(int j=0;j<=n;j++){
+                dp[i][j]=-1;
+            }
         }
-    }
-    return paths(m,n);
+        return paths(m,n);
     }
 }
